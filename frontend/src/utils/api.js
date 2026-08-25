226524a2433643945ed40208373ec7465a924aca
Akshayreddy2004 <akshaychalla0923@gmail.com>
2026-08-25 16:53:07 +0530

    Make API base URL configurable via VITE_API_BASE

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const BASE = import.meta?.env?.VITE_API_BASE || 'http://localhost:8000/api';
+
 const api = axios.create({
-  baseURL: 'https://syam-infra-cms-0pc3.onrender.com/api',
+  baseURL: BASE,
 });
 
 api.interceptors.request.use(
